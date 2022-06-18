e7f4a7d7c6898a6eb807c7fddae96030391d96d7
ardiyamalik <malikardiya1@gmail.com>
2022-06-18 17:46:45 +0700

    latihan commit

--- halamanapi.php
+++ halamanapi.php
@@ -92,10 +92,10 @@ if( empty($_SESSION['uname']) ){
                     </li>
                     <br>
                     <li class="nav-item">
-                        <p><strong>NAMA KALIAN</strong></p>
+                        <p><strong>Ardiya Malik Jaelani</strong></p>
                     </li>
                     <li class="nav-item">
-                        <p>NIM KALIAN</p>
+                        <p>6706210140</p>
                     </li>
                     </li>
                 </ul>
